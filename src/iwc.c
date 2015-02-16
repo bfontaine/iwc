e22c9846940a30d2f17b3fb45bd0c598856a7e36
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-16 10:35:35 +0100

    minor speed improvements

--- src/iwc.c
+++ src/iwc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "iwc.h"
 #include "version.h"
@@ -11,13 +12,13 @@ char buf[BUF_SIZE];
 
 void iwc_count_lines(int buflen, int *lines) {
         int _lines = 0;
+        char *p = buf;
 
         if (lines == NULL) { return; }
 
-        for (int i=0; i<buflen; i++) {
-                if (buf[i] == '\n') {
-                        _lines += 1;
-                }
+        while ((p = memchr(p, '\n', (buf + buflen) - p))) {
+                ++p;
+                ++_lines;
         }
 
         *lines += _lines;
@@ -29,13 +30,19 @@ void iwc_count_words(int buflen, int *words) {
 
         if (words == NULL) { return; }
 
-        for (int i=0; i<buflen; i++) {
+        for (int i=0; i<buflen; ++i) {
                 char c = buf[i];
 
                 // really simple, just for testing
-                if (!last_was_a_space && (c == '\n' || c == '\r' || c == ' ')) {
+                if (!last_was_a_space &&
+                                (c == '\n' ||
+                                 c == '\r' ||
+                                 c == '\t' ||
+                                 c == '\v' ||
+                                 c == '\f' ||
+                                 c == ' ')) {
                         last_was_a_space = 1;
-                        _words += 1;
+                        ++_words;
                 } else {
                         last_was_a_space = 0;
                 }
@@ -52,9 +59,9 @@ void iwc_count_bytes(int buflen, int *bytes) {
 
 void iwc_print_counter(int *lines, int *words, int *bytes, char eol) {
         char printed = 0;
-        if (lines != NULL) { printf("%d ", *lines); printed = 1; }
-        if (words != NULL) { printf("%d ", *words); printed = 1; }
-        if (bytes != NULL) { printf("%d ", *bytes); printed = 1; }
+        if (lines != NULL) { printf(" %7d", *lines); printed = 1; }
+        if (words != NULL) { printf(" %7d", *words); printed = 1; }
+        if (bytes != NULL) { printf(" %7d", *bytes); printed = 1; }
 
         if (printed) {
                 printf("%c", eol);
